d46034bfa2d81d87925196e6268fc8a9d8eca9d7
kevenn <kevengodinho3474@gmail.com>
2026-04-07 10:23:54 -0300

    Correção de erros no ProdutosService

--- src/main/java/br/com/keven/spring_boot_essentials/service/ProdutoService.java
+++ src/main/java/br/com/keven/spring_boot_essentials/service/ProdutoService.java
@@ -1,6 +1,6 @@
 package br.com.keven.spring_boot_essentials.service;
+
 import br.com.keven.spring_boot_essentials.database.model.ProdutoEntity;
-import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -8,44 +8,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
-
 public class ProdutoService {
 
-    private final ProdutoEntity produtoEntity;
+    private static final List<ProdutoEntity> PRODUTOS = new ArrayList<>();
 
-    public ProdutoService( ProdutoEntity produtoEntity ){
-        this.produtoEntity = produtoEntity;
+    static {
+        ProdutoEntity p = new ProdutoEntity();
+        p.setId(1);
+        p.setName("Notebook");
+        p.setPreco(new BigDecimal("5000"));
+        p.setQuantidade(10);
+
+        ProdutoEntity p1 = new ProdutoEntity();
+        p1.setId(2);
+        p1.setName("ipohne");
+        p1.setPreco(new BigDecimal("7000"));
+        p1.setQuantidade(10);
+
+
+        ProdutoEntity p2 = new ProdutoEntity();
+        p2.setId(3);
+        p2.setName("mouse");
+        p2.setPreco(new BigDecimal("500"));
+        p2.setQuantidade(10);
+
+
+        PRODUTOS.add(p);
+        PRODUTOS.add(p2);
+        PRODUTOS.add(p1);
     }
-
-    ProdutoEntity p = new ProdutoEntity();
-            p.setId(1);
-            p.setName("Notebook");
-            p.setPreco(new BigDecimal("5000"));
-            p.setQuantidade(10);
-
-
-    /*private static final List<ProdutoEntity> PRODUTOS = List.of(
-            ProdutoEntity.builder()
-                    .id(1)
-                    .name("Notebook")
-                    .preco(new BigDecimal("5000"))
-                    .quantidade(10)
-                    .build(),
-
-            ProdutoEntity.builder()
-                    .id(2)
-                    .name("Iphone")
-                    .preco(new BigDecimal("7000"))
-                    .quantidade(10)
-                    .build(),
-
-            ProdutoEntity.builder()
-                    .id(3)
-                    .name("Mouse")
-                    .preco(new BigDecimal("500"))
-                    .quantidade(10)
-                    .build()
-    );*/
 
     public List<ProdutoEntity> findAll(){
         return new ArrayList<>(PRODUTOS);
